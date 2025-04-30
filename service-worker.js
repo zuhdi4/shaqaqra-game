8d63629c2cfe0b92dde74d112b87ec942db8a176
zuhdi4 <zuhdi4@gmail.com>
2025-04-30 23:22:24 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('shaqaqra-v1').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/game.html'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
